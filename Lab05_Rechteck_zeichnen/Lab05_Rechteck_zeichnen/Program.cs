80d9fda4f4c025be7e4a0a42861249befd16fdb1
Adrian Diab <adrian.diab@htlwy.at>
2026-01-23 11:18:58 +0100

    Lab05_Rechteck_zeichnen fertiggestellt

---
+++ Lab05_Rechteck_zeichnen/Lab05_Rechteck_zeichnen/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab05_Rechteck_zeichnen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bitte breite eingeben: \n");
+            int breite = int.Parse(Console.ReadLine());
+
+            Console.Write("Bitte hoehe eingeben: \n");
+            int hoehe = int.Parse(Console.ReadLine());
+
+
+            // Rechteck-Objekt erstellen
+            Rechteck meinRechteck = new Rechteck(breite, hoehe);
+
+            // Rechteck zeichnen
+            meinRechteck.Zeichnen();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab05_Rechteck_zeichnen/Lab05_Rechteck_zeichnen/Rechteck.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab05_Rechteck_zeichnen
+{
+    internal class Rechteck
+    {
+        // Attribute
+        private int breite;
+        private int hoehe;
+        // Konstruktor
+        public Rechteck(int breite, int hoehe)
+        {
+            this.breite = breite;
+            this.hoehe = hoehe;
+        }
+        // Methode zum Zeichnen des Rechtecks
+        public void Zeichnen()
+        {
+            for (int j = 0; j < breite; j++)
+            {
+                Console.Write("*");
+                
+            }
+            Console.WriteLine();
+            for (int i = 0; i < hoehe - 2; i++)
+            {
+                Console.Write("*");
+                for (int j = 0; j < breite - 2; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("*");
+            }
+
+            for (int j = 0; j < breite; j++)
+            {
+                Console.Write("*");
+
+            }
+        }
+    }
+}
