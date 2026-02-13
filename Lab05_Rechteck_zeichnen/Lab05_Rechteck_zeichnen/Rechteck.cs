68f8d2ca7d112d4d0a98113103562bc0a96629b6
Adrian Diab <adrian.diab@htlwy.at>
2026-02-13 11:58:47 +0100

    Labyrinth gezeichnet mit WPF

--- Lab05_Rechteck_zeichnen/Lab05_Rechteck_zeichnen/Rechteck.cs
+++ Lab05_Rechteck_zeichnen/Lab05_Rechteck_zeichnen/Rechteck.cs
@@ -92,5 +92,5 @@ namespace Lab05_Rechteck_zeichnen
             }
         }
     }
-}
+} 
 
---
+++ Lab06_Labyrinth_WPF/Example/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Example
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            StreamReader reader = new StreamReader("maze_6x6.txt");
+            string inhalt = reader.ReadToEnd();
+            string[] zeilen = inhalt.Split('\n');
+
+            this.Spielfeld.Background = Brushes.Aquamarine;
+
+            for (int a = 0; a < zeilen.Length; a++)
+            {
+                for (int i = 0; i < zeilen[a].Length; i++)
+                {
+                    if (zeilen[a][i] == '#')
+                    {
+                        Canvas c = new Canvas();
+                        c.Background = Brushes.Red;
+                        c.Width = 20;
+                        c.Height = 20;
+                        Canvas.SetTop(c, a * 20);
+                        Canvas.SetLeft(c, i * 20);
+                        Spielfeld.Children.Add(c);
+                    }
+                }
+            }
+        }
+    }
+}
